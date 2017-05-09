c3ca581c991bca6206cd152a299ec4e6653633d7
phillipf <phillip-f@live.com>
2017-05-09 10:52:56 +1000

    update getBCreport function
    add in a fallback to get most recent report if today is not available

--- R/getBCreport.R
+++ R/getBCreport.R
@@ -9,10 +9,27 @@
 
 getBCreport <- function() {
 
+  files <- list.files("N:/FWBI/SDCanalysis/Reports/PermanentBCreport", full.names = TRUE)
+
   today <- format(Sys.Date(), "%d%m%Y")
 
   filename <- paste("N:/FWBI/SDCanalysis/Reports/PermanentBCreport/", today, "_BodyCorpSDC.csv", sep = "")
 
-  readr::read_csv(filename)
+  if(filename %in% files) {
 
+    df <- readr::read_csv(filename)
+
+    }
+
+  else {
+
+    reports <- files[grepl("BodyCorpSDC", files)]
+
+    ord <- sort.list(as.Date(gsub(".*\\/([0-9]*)_.*$","\\1",reports), format = "%d%m%Y"), decreasing = F)
+
+    df <- readr::read_csv(tail(reports[ord], 1))
+
+  }
+
+  return(df)
 }
